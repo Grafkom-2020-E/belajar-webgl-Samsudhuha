332c59891e9c34601ab439821071a181c30dc819
Samsudhuha <msamsudhuha9@gmail.com>
2020-10-16 09:13:53 +0800

    Gambar Segitiga

--- M1/index.js
+++ M1/index.js
@@ -1,48 +1,47 @@
 function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
-  
+
     var vertexShaderSource = `
     void main() {
         gl_PointSize = 10.0;
         gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
     }
     `;
-  
+
     var fragmentShaderSource = `
     void main() {
         gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
     }
     `;
-  
+
     // Buat .c di GPU
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(vertexShader, vertexShaderSource);
     gl.shaderSource(fragmentShader, fragmentShaderSource);
-  
+
     // Kompilasi .c agar menjadi .o
     gl.compileShader(vertexShader);
     gl.compileShader(fragmentShader);
-  
+
     // Siapkan wadah untuk .exe (shader program)
     var shaderProgram = gl.createProgram();
-  
+
     // Masukkan .o ke dalam wadah .exe
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
-  
+
     // Sambungkan antar .o agar bisa menjadi
     // runnable context di dalam wadah .exe tadi
     gl.linkProgram(shaderProgram);
-  
+
     // Mulai menggunakan konteks (cat)
     gl.useProgram(shaderProgram);
-  
+
     // Mewarnai background
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.drawArrays(gl.POINTS, 0, 1);
-  }
-  
+}
